0bbc69e911cd168495c7fb60c6a445c056236e14
hyun1014 <ksp70712@gmail.com>
2020-11-23 21:42:13 +0900

    Bsearch, upper/lower bound

--- binary_search.cpp
+++ binary_search.cpp
@@ -1,50 +1,53 @@
 #include <stdio.h>
+#include <algorithm>
+#include <vector>
+#include <queue>
 
 using namespace std;
 
-int binary_search(int arr[], int len, int tar){
-    int s = 0;
-    int e = len-1;
+int binary_search(int* arr, int start, int end, int tar){
     int mid;
-    while(s<=e){
-        mid = (s + e) / 2;
+    while(start<=end){
+        mid = (start + end) / 2;
         if(arr[mid]==tar)
             return mid;
         else if (arr[mid]<tar)
-            s = mid + 1;
+            start = mid+1;
         else
-            e = mid - 1;
+            end = mid-1;
     }
     return -1;
 }
-int lower_bound(int arr[], int len, int tar){
-    int s = 0, e = len, mid = 0;
-    while(s<e){
-        mid = (s + e) / 2;
-        if(arr[mid]<tar)
-            s = mid + 1;
-        else
-            e = mid;
-    }
-    return e;
-}
-int upper_bound(int arr[], int len, int tar){
-    int s = 0, e = len, mid = 0;
-    while(s<e){
-        mid = (s + e) / 2;
+int upperBound(int* arr, int len, int tar){
+    int start = 0, end = len;
+    int mid;
+    while(start<end){
+        mid = (start + end) / 2;
         if(arr[mid]<=tar)
-            s = mid + 1;
+            start = mid + 1;
         else
-            e = mid;
+            end = mid;
     }
-    return e;
+    return start;
 }
+int lowerBound(int* arr, int len, int tar){
+    int start = 0, end = len;
+    int mid;
+    while(start<end){
+        mid = (start + end) / 2;
+        if(arr[mid]<tar)
+            start = mid + 1;
+        else
+            end = mid;
+    }
+    return start;
+}
+
 
 int main(void){
-    int arr[10] = {1,3,5,7,9,11,13,15,17,19};
-    printf("%d\n", binary_search(arr, sizeof(arr)/4, 20));
-    printf("%d\n", lower_bound(arr, sizeof(arr)/4, 12));
-    printf("%d\n", upper_bound(arr, sizeof(arr)/4, 12));
+    int arr[12] = {1,1,3,3,5,5,7,7,7,7,9,9};
+    // int arr[12] = {1,2,3,4,5,6,7,8,9,10,11,12};
+    printf("%d\n", upperBound(arr, 12, 7));
+    printf("%d\n", lowerBound(arr, 12, 7));
     return 0;
 }
-
