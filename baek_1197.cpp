d8b738f6629a92a40712c7b2d945ccf86afaf4ac
hyun1014 <ksp70712@gmail.com>
2020-11-23 21:15:48 +0900

    Minimum Spanning Tree

--- baek_1197.cpp
+++ baek_1197.cpp
@@ -1,58 +1,67 @@
-#include <iostream>
-#include <vector>
+#include <stdio.h>
 #include <algorithm>
+#include <vector>
 #include <queue>
-#pragma warning(disable : 4996)
 
 using namespace std;
-int* parent = new int[10001];
 
-struct Edge {
-	int from;
-	int to;
-	int weight;
-};
+vector<int> parent;
+vector<int> nodeCount;
+
+int find(int x){
+    if(parent[x]==x)
+        return x;
+    else{
+        parent[x] = find(parent[x]);
+        return parent[x];
+    }
+}
+
+void union_set(int x, int y){
+    int xp = find(x);
+    int yp = find(y);
+    parent[yp] = xp;
+    nodeCount[xp] += nodeCount[yp];
+}
+
+bool check(int x, int y){
+    return find(x)==find(y);
+}
+
 struct cmp {
-	bool operator()(Edge t, Edge u) {
-		return t.weight > u.weight;
-	}
+    bool operator()(pair<pair<int,int>,int> a, pair<pair<int,int>,int> b){
+        if(a.second==b.second){
+            return a.first.first > b.first.first;
+        }
+        else{
+            return a.second > b.second;
+        }
+    }
 };
 
-int find(int u) {
-	if (u == parent[u])
-		return u;
-	return parent[u] = find(parent[u]);
-}
-
-void merge_k(int a, int b) {
-	parent[find(a)] = find(b);
-}
-
-int main() {
-	Edge ed;
-	priority_queue<Edge, vector<Edge>, cmp> que;
-	int v, e, a, b, c, i, sum;
-	Edge temp;
-	sum = 0;
-	scanf("%d %d", &v, &e);
-	for (i = 1; i <= v; i++)
-		parent[i] = i;
-
-	for (i = 0; i < e; i++) {
-		scanf("%d %d %d", &a, &b, &c);
-		ed.from = a;
-		ed.to = b;
-		ed.weight = c;
-		que.push(ed);
-	}
-	while (!que.empty()) {
-		temp = que.top();
-		que.pop();
-		if (find(temp.from) != find(temp.to)) {
-			sum += temp.weight;
-			merge_k(temp.from, temp.to);
-		}
-	}
-	printf("%d\n", sum);
-	return 0;
+int main(void){
+    int v, e, a, b, c;
+    int ans = 0;
+    priority_queue<pair<pair<int,int>,int>, vector<pair<pair<int,int>,int>>, cmp> q;
+    scanf("%d %d", &v, &e);
+    for(int i=0; i<=v; i++){
+        parent.push_back(i);
+        nodeCount.push_back(1);
+    }
+    for(int i=0; i<e; i++){
+        scanf("%d %d %d", &a, &b, &c);
+        q.push(make_pair(make_pair(a, b), c));
+    }
+    while(!q.empty()){
+        pair<pair<int,int>,int> cur = q.top();
+        q.pop();
+        if(!check(cur.first.first, cur.first.second)){
+            ans += cur.second;
+            union_set(cur.first.first, cur.first.second);
+            if(nodeCount[cur.first.first]==v)
+                break;
+        }
+    }
+    printf("%d\n", ans);
+    return 0;
 }
