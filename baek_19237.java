f13ca943b4a91a9c0022ea324cad0c8b7fa59491
hyun1014 <ksp70712@gmail.com>
2021-12-07 00:37:46 +0900

    Simulation

--- baek_19237.java
+++ baek_19237.java
@@ -7,24 +7,34 @@ import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int n, m, k;
+    static int n;
+    static int m;
+    static int k;
     static int[] dx = new int[] {0, -1, 1, 0, 0};
     static int[] dy = new int[] {0, 0, 0, -1, 1};
-    static int[][] smellCount;
-    static int[][] smellCountTemp;
-    static int[][] smellShark;
-    static int[][] smellSharkTemp;
-    static Shark[] sharkList;
-    static int[] directionOfShark;
-    static int[] pointOfShark;
-    static Set<Integer> sharkSet = new HashSet<>();
+    static Shark[] priorityOfSharks;
+    static Set<Integer> setOfRemainingShark = new HashSet<>();
+
+    static Smell[][] smellBoard;
+    static int[][] sharkBoard;
+    static int[][] sharkBoardTemp;
+    static int[] directionOfSharks;
+    static int[] positionOfSharks;
+
+    static class Smell {
+        int count;
+        int sharkNumber;
+        Smell(int count, int sharkNumber) {
+            this.count = count;
+            this.sharkNumber = sharkNumber;
+        }
+    }
     static class Shark {
-        int[][] priority = new int[5][4];
-        void setPriority(int idx, String info) {
-            StringTokenizer st = new StringTokenizer(info);
-            for (int i = 0; i < 4; i++) {
-                priority[idx][i] = Integer.parseInt(st.nextToken());
-            }
+        int[][] priorityOfMovement = new int[5][4];
+        void setPriorityOfMovement(int idx, String priorityString) {
+            StringTokenizer st = new StringTokenizer(priorityString);
+            for (int i = 0; i < 4; i++)
+                priorityOfMovement[idx][i] = Integer.parseInt(st.nextToken());
         }
     }
 
@@ -32,42 +42,39 @@ public class Main {
         return x < 0 || x >= n || y < 0 || y >= n;
     }
 
-    static void moveShark() {
-        for (int i = sharkList.length - 1; i > 0; i--) {
-            boolean isMoved = false;
-            if (sharkSet.contains(i)) {
-                Shark currentShark = sharkList[i];
-                int x = pointOfShark[i] / 100;
-                int y = pointOfShark[i] % 100;
-                int[] priorityOfDirection = currentShark.priority[directionOfShark[i]];
-                for (int j = 0; j < 4; j++) {
-                    int fx = x + dx[priorityOfDirection[j]];
-                    int fy = y + dy[priorityOfDirection[j]];
-                    if (!isOutOfBound(fx, fy) && smellCount[fx][fy] == 0) {
-                        if (smellSharkTemp[fx][fy] != 0) {
-                            sharkSet.remove(smellSharkTemp[fx][fy]);
-                        }
-                        smellCountTemp[fx][fy] = k;
-                        smellSharkTemp[fx][fy] = i;
-                        pointOfShark[i] = fx * 100 + fy;
-                        directionOfShark[i] = priorityOfDirection[j];
-                        isMoved = true;
-                        break;
-                    }
+    static void moveShark(int sharkNumber) {
+        int cx = positionOfSharks[sharkNumber] / 100;
+        int cy = positionOfSharks[sharkNumber] % 100;
+        int currentDirection = directionOfSharks[sharkNumber];
+        int[] priorityOfMovement = priorityOfSharks[sharkNumber].priorityOfMovement[currentDirection];
+        boolean isMoved = false;
+        for (int idx = 0; idx < 4; idx++) {
+            int dir = priorityOfMovement[idx];
+            int fx = cx + dx[dir];
+            int fy = cy + dy[dir];
+            if (!isOutOfBound(fx, fy) && smellBoard[fx][fy] == null) {
+                if (sharkBoardTemp[fx][fy] != 0) {
+                    setOfRemainingShark.remove(sharkBoardTemp[fx][fy]);
                 }
-                if (!isMoved) {
-                    for (int j = 0; j < 4; j++) {
-                        int fx = x + priorityOfDirection[j];
-                        int fy = y + priorityOfDirection[j];
-                        if (!isOutOfBound(fx, fy) && smellShark[fx][fy] == i) {
-                            smellCountTemp[fx][fy] = k;
-                            smellSharkTemp[fx][fy] = i;
-                            pointOfShark[i] = fx * 100 + fy;
-                            directionOfShark[i] = priorityOfDirection[j];
-                            break;
-                        }
-                    }
-                }
+                sharkBoardTemp[fx][fy] = sharkNumber;
+                positionOfSharks[sharkNumber] = fx * 100 + fy;
+                directionOfSharks[sharkNumber] = dir;
+                isMoved = true;
+                break;
+            }
+        }
+        if (isMoved)
+            return;
+
+        for (int idx = 0; idx < 4; idx++) {
+            int dir = priorityOfMovement[idx];
+            int fx = cx + dx[dir];
+            int fy = cy + dy[dir];
+            if (!isOutOfBound(fx, fy) && smellBoard[fx][fy].sharkNumber == sharkNumber) {
+                sharkBoardTemp[fx][fy] = sharkNumber;
+                positionOfSharks[sharkNumber] = fx * 100 + fy;
+                directionOfSharks[sharkNumber] = dir;
+                break;
             }
         }
     }
@@ -75,73 +82,87 @@ public class Main {
     static void decreaseSmell() {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                smellCount[i][j]--;
-                if (smellCount[i][j] <= 0) {
-                    smellCount[i][j] = 0;
-                    smellShark[i][j] = 0;
+                if (smellBoard[i][j] != null) {
+                    smellBoard[i][j].count--;
+                    if (smellBoard[i][j].count == 0)
+                        smellBoard[i][j] = null;
                 }
             }
         }
     }
 
-    static void clearBoard() {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (smellSharkTemp[i][j] != 0) {
-                    smellShark[i][j] = smellSharkTemp[i][j];
-                    smellSharkTemp[i][j] = 0;
-                }
-                if (smellCountTemp[i][j] != 0) {
-                    smellCount[i][j] = smellCountTemp[i][j];
-                    smellCountTemp[i][j] = 0;
-                }
+    static void updateBoard() {
+        for (int sharkNumber = m; sharkNumber > 0; sharkNumber--) {
+            if (setOfRemainingShark.contains(sharkNumber)) {
+                smellBoard[positionOfSharks[sharkNumber]/100][positionOfSharks[sharkNumber]%100] = new Smell(k, sharkNumber);
             }
         }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                sharkBoard[i][j] = sharkBoardTemp[i][j];
+                sharkBoardTemp[i][j] = 0;
+            }
+        }
+    }
+
+    static void doProcess() {
+        for (int currentSharkNumber = m; currentSharkNumber > 0; currentSharkNumber--) {
+            if (!setOfRemainingShark.contains(currentSharkNumber)) {
+                continue;
+            }
+            moveShark(currentSharkNumber);
+        }
+        decreaseSmell();
+        updateBoard();
     }
 
     public static void main(String[] args) throws Exception {
         int answer = 0;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
-        smellCount = new int[n][n];
-        smellCountTemp = new int[n][n];
-        smellShark = new int[n][n];
-        smellSharkTemp = new int[n][n];
-        sharkList = new Shark[m + 1];
-        pointOfShark = new int[m + 1];
-        directionOfShark = new int[m + 1];
+
+        smellBoard = new Smell[n][n];
+        sharkBoard = new int[n][n];
+        sharkBoardTemp = new int[n][n];
+        priorityOfSharks = new Shark[m + 1];
+        directionOfSharks = new int[m + 1];
+        positionOfSharks = new int[m + 1];
 
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < n; j++) {
-                smellShark[i][j] = Integer.parseInt(st.nextToken());
-                if (smellShark[i][j] != 0) {
-                    smellCount[i][j] = k;
-                    pointOfShark[smellShark[i][j]] = i * 100 + j;
+                int sharkNumber = Integer.parseInt(st.nextToken());
+                if (sharkNumber != 0) {
+                    priorityOfSharks[sharkNumber] = new Shark();
+                    positionOfSharks[sharkNumber] = i * 100 + j;
+                    setOfRemainingShark.add(sharkNumber);
+                    smellBoard[i][j] = new Smell(k, sharkNumber);
                 }
             }
         }
 
         st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= m; i++)
-            directionOfShark[i] = Integer.parseInt(st.nextToken());
+        for (int sharkNumber = 1; sharkNumber <= m; sharkNumber++)
+            directionOfSharks[sharkNumber] = Integer.parseInt(st.nextToken());
 
-        for (int i = 1; i <= m; i++) {
-            sharkList[i] = new Shark();
-            sharkSet.add(i);
-            for (int j = 1; j <= 4; j++)
-                sharkList[i].setPriority(j, br.readLine());
+        for (int sharkNumber = 1; sharkNumber <= m; sharkNumber++) {
+            for (int direction = 1; direction <= 4; direction++) {
+                priorityOfSharks[sharkNumber].setPriorityOfMovement(direction, br.readLine());
+            }
         }
 
         do {
             answer++;
-            moveShark();
-            decreaseSmell();
-            clearBoard();
-        } while (sharkSet.size() > 1);
+            if (answer > 1000) {
+                answer = -1;
+                break;
+            }
+            doProcess();
+        } while (setOfRemainingShark.size() > 1);
 
         System.out.println(answer);
 
