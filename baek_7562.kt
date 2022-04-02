71a83152970184e8831f97b44c79d9ea59de281c
hyun1014 <ksp70712@gmail.com>
2022-04-02 15:50:27 +0900

    using kotlin

--- baek_7562.kt
+++ baek_7562.kt
@@ -1,40 +1,39 @@
 package com.study
 
-import java.io.BufferedReader
-import java.io.InputStreamReader
 import java.util.LinkedList
 
+val br by lazy { System.`in`.bufferedReader() }
+
 fun main(args: Array<String>) {
-    val br = BufferedReader(InputStreamReader(System.`in`))
     val testCount = br.readLine().toInt()
     for (tcase in 0 until testCount) {
         val l = br.readLine().toInt()
         val start = br.readLine().split(" ").map { it.toInt() }
         val target = br.readLine().split(" ").map { it.toInt() }
-        println(findDepth(Pair(start[0], start[1]), Pair(target[0], target[1]), l))
+        println(findDepth(start[0] to start[1], target[0] to target[1], l))
     }
 }
 
-fun findDepth(start: Pair, target: Pair, l: Int): Int {
-    val dx = arrayOf(-1, -2, -2, -1, 1, 2, 2, 1)
-    val dy = arrayOf(-2, -1, 1, 2, 2, 1, -1, -2)
+fun findDepth(start: Pair<Int, Int>, target: Pair<Int, Int>, l: Int): Int {
+    val moving = arrayOf(-2 to -1, -1 to -2, 1 to -2, 2 to -1, 2 to 1, 1 to 2, -1 to 2, -2 to 1)
     val visitedSet = mutableSetOf<Int>()
-    val queue = LinkedList<Pair>()
+    val queue = LinkedList<Pair<Int, Int>>()
     var depth = 0
     var prev = 1
     var next = 0
     queue.add(start)
-    visitedSet.add(start.x * 1000 + start.y)
+    visitedSet.add(start.first * 1000 + start.second)
 
-    while (!queue.isEmpty()) {
+
+    while (queue.isNotEmpty()) {
         val current = queue.poll()
         prev--
         if (current.equals(target)) {
             break
         }
-        for (i in 0 until 8) {
-            val nx = current.x + dx[i]
-            val ny = current.y + dy[i]
+        for ((dx, dy) in moving) {
+            val nx = current.first + dx
+            val ny = current.second + dy
             if (nx in 0 until l && ny in 0 until l && !visitedSet.contains(nx * 1000 + ny)) {
                 queue.add(Pair(nx, ny))
                 visitedSet.add(nx * 1000 + ny)
@@ -49,5 +48,3 @@ fun findDepth(start: Pair, target: Pair, l: Int): Int {
     }
     return depth
 }
-
-data class Pair(val x: Int, val y: Int)
